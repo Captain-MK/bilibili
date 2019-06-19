f4c73830f79c657f727a75cb9219952d7b13798c
mengkai <476618228@qq.com>
2019-06-19 18:18:24 +0800

    我的页面nav渐变色

--- flutter_bilibili/lib/Fragment/my_Page.dart
+++ flutter_bilibili/lib/Fragment/my_Page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bilibili/AppModel.dart';
 import '../Fragment/animationPage.dart';
 class myPage extends StatefulWidget {
   @override
@@ -6,47 +7,92 @@ class myPage extends StatefulWidget {
 }
 
 class _myPageState extends State<myPage> {
+  double _NavHeight = 97.0;
   var _listController;
-  var _show = true;
+  var _opacity = 0.0;
+
+
+  var _index=0;
+  var _themeColor;
+  void getindex ()async{
+    _index = await AppModel().getSaveIndex();
+  }
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _listController = ScrollController();
+    getindex();
+  }
+  Widget itemWidget(int index) {
+    if (index == 0){
+      return Container(
+        height: _NavHeight,
+        child: Image.asset('images/bj.png',fit: BoxFit.fill,),
+      );
+    }else{
+      return ListTile(
+        title: Text('今天吃什么？${index}',style: TextStyle(color: _themeColor),),
+        leading: Icon(Icons.query_builder,color: _themeColor,),
+        onTap: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (context)=>animationPage()));
+        },
+      );
+    }
+  }
+  void _onScroller(int i) {
+    var aa = i/_NavHeight;
+    if(aa >= 1.0){
+      aa = 1.0;
+    }else if (aa <=0.0){
+      aa = 0.0;
+    }
+    setState(() {
+      _opacity = aa;
+    });
   }
   @override
   Widget build(BuildContext context) {
+    final model = AppModel.of(context);
+    _themeColor = themeList[model.themeIndex != 0?model.themeIndex:_index];
     return Scaffold(
-      appBar: _show?AppBar(
-        title: Text('我的'),
-        brightness: Brightness.light,
-        centerTitle: true,
-      ):null,
-      body: NotificationListener(
-        onNotification: (ScrollNotification note) {
-          print(note.metrics.pixels.toInt());// 滚动位置。
-          if (note.metrics.pixels.toInt() > 50){
-            setState(() {
-              _show = false;
-            });
-          }else{
-            setState(() {
-              _show = true;
-            });
-          }
-        },
-        child: new ListView.builder(
-          itemCount: 20,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text('今天吃什么？${index}'),
-              leading: Icon(Icons.select_all),
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>animationPage()));
-              },
-            );
-          },
-        ),
+      body: Stack(
+        children: <Widget>[
+          NotificationListener(
+            onNotification: (ScrollNotification note) {
+              print(note.metrics.pixels.toInt());// 滚动位置。
+              _onScroller(note.metrics.pixels.toInt());
+            },
+            child: MediaQuery.removePadding(
+              context: context,
+              removeTop: true,
+              child: ListView.builder(
+                itemCount: 30,
+                itemBuilder: (BuildContext context, int index) {
+                  return itemWidget(index);
+                },
+              ),
+            ),
+          ),
+          Opacity(
+            opacity: _opacity,
+            child: Container(
+              height: _NavHeight,
+              color: _themeColor,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Text('我的页面',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18.0),),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- flutter_bilibili/lib/Fragment/shoppingPage/changePage.dart
+++ flutter_bilibili/lib/Fragment/shoppingPage/changePage.dart
@@ -1,38 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bilibili/AppModel.dart';
+
 class changePage extends StatefulWidget {
   @override
   _changePageState createState() => _changePageState();
 }
 
 class _changePageState extends State<changePage> {
-  Widget items (BuildContext c,int index){
+  Widget items(BuildContext c, int index) {
     final appmodel = AppModel.of(context);
     return InkWell(
       child: Container(
         height: 150.0,
         color: themeList[index],
         child: Center(
-          child: Text(index.toString(),style: TextStyle(fontSize: 25.0,color: Colors.white),),
+          child: Text(
+            index.toString(),
+            style: TextStyle(fontSize: 25.0, color: Colors.white),
+          ),
         ),
       ),
-      onTap: (){
+      onTap: () {
         appmodel.changeTheme(index);
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('颜色切换'),),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: ListView.builder(
-            itemCount: themeList.length,
-            itemBuilder: (c,index){
-              return items(c,index);
-            }),
+      appBar: AppBar(
+        title: Text('颜色切换'),
       ),
+      body: WillPopScope(//可以取消测滑返回
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: ListView.builder(
+                itemCount: themeList.length,
+                itemBuilder: (c, index) {
+                  return items(c, index);
+                }),
+          ),
+          onWillPop: () {
+            Navigator.of(context).pop();
+          }),
     );
   }
 }
